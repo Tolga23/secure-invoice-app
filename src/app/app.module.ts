7e98ad838ac2a7a18b0c91b1eaf0efb8390d7cd8
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-11-10 23:08:56 +0300

    Added login, register, verify and resetpassword pages
    Also PrimeNG native Angular UI component added.
    npm i primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VerifyComponent } from './pages/verify/verify.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ResetpasswordComponent } from './pages/resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VerifyComponent,
+    RegisterComponent,
+    LoginComponent,
+    ResetpasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
