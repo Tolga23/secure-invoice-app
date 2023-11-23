e8bab0710f446f1ec317a394867771d45966c4ed
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-11-23 06:21:31 +0300

    Add profile method service and Updated profile components
    Add profile and updateProfile methods in user.service
    Add State interface
    Updated profile components

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {Router} from "@angular/router";
+import {UserService} from "../../service/user.service";
+import {DataState} from "../../enum/datastate.enum";
+import {State} from "../../interface/state";
+import {CustomHttpResponse} from "../../interface/customhttpresponse";
+import {Profile} from "../../interface/profile";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileState$: Observable<State<CustomHttpResponse<Profile>>>
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Profile>>(null)
+  private isLoadingSubject = new BehaviorSubject<boolean>(false)
+  isLoading$ = this.isLoadingSubject.asObservable()
+  readonly DataState = DataState
+
+  constructor(private router: Router, private userService: UserService) {
+  }
+
+
+  /**
+   * ngOnInit is a lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+   * Here, it is used to initialize the profileState$ observable.
+   *
+   * @returns {void}
+   */
+  ngOnInit(): void {
+    this.profileState$ = this.userService.profile$()
+      .pipe(
+        map(response => {
+          console.log(response)
+          this.dataSubject.next(response)
+          return {dataState: DataState.LOADED, appData: response}
+        }),
+        startWith({dataState: DataState.LOADING}),
+        catchError((error: string) => {
+          return of({dataState: DataState.ERROR, appData: this.dataSubject.value, error})
+        })
+      )
+  }
+
+  updateProfile(profileForm: NgForm): void {
+    this.isLoadingSubject.next(true)
+    this.profileState$ = this.userService.updateProfile$(profileForm.value)
+      .pipe(
+        map(response => {
+          console.log(response)
+          this.dataSubject.next({...response, data: response.data})
+          this.isLoadingSubject.next(false)
+          return {dataState: DataState.LOADED, appData: this.dataSubject.value}
+        }),
+        startWith({dataState: DataState.LOADED, appData: this.dataSubject.value}),
+        catchError((error: string) => {
+          this.isLoadingSubject.next(false)
+          return of({dataState: DataState.LOADED, appData: this.dataSubject.value, error})
+        })
+      )
+  }
 
 }
+
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable, tap, throwError} from "rxjs";
 import {CustomHttpResponse} from "../interface/customhttpresponse";
 import {Profile} from "../interface/profile";
+import {User} from "../interface/user";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,16 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
 
+  /**
+   * This method is used to authenticate a user.
+   * It sends a POST request to the '/login' endpoint with the user's email and password.
+   *
+   * @param {string} email - The email of the user.
+   * @param {string} password - The password of the user.
+   *
+   * @returns {Observable<CustomHttpResponse<Profile>>} An Observable that will emit the server's response.
+   * The response is expected to be of type `CustomHttpResponse<Profile>`.
+   */
   login$ = (email: string, password: string) => <Observable<CustomHttpResponse<Profile>>>
     this.http.post<CustomHttpResponse<Profile>>
     (`${this.baseUrl}/login`, {email, password})
@@ -28,6 +39,24 @@ export class UserService {
         tap(console.log),
         catchError(this.handleError)
       );
+
+  profile$ = () => <Observable<CustomHttpResponse<Profile>>>
+    this.http.get<CustomHttpResponse<Profile>>
+    (`${this.baseUrl}/profile`, {headers: new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHRVRfU0VDVVJFIiwiYXVkIjoiQ1VTVE9NX01BTkFHRU1FTlRfU0VSVklDRSIsImlhdCI6MTcwMDcwODI2OCwic3ViIjoiNjAyIiwiYXV0aG9yaXRpZXMiOlsiUkVBRDpVU0VSIiwiUkVBRDpDVVNUT01FUiIsIkNSRUFURTpVU0VSIiwiQ1JFQVRFOkNVU1RPTUVSIiwiVVBEQVRFOlVTRVIiLCJVUERBVEU6Q1VTVE9NRVIiLCJERUxFVEU6VVNFUiIsIkRFTEVURTpDVVNUT01FUiJdLCJleHAiOjE3MDA3MTAwNjh9.wQ9Aqfzo4vmADrjy7IUYhDm5viAqdwCOFx6miXPzirSZZAOJLrJSsDffaykeCvM1JyNRNpnb6-LKZrXF677qhw')})
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      )
+
+
+  updateProfile$ = (user: User) => <Observable<CustomHttpResponse<Profile>>>
+    this.http.patch<CustomHttpResponse<Profile>>
+    (`${this.baseUrl}/update`, user)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      )
+
 
   private handleError(error: HttpErrorResponse): Observable<never> {
     let errorMessage: string
