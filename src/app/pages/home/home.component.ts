908cf5c5382602ffcff519035a46ce41da81f6b8
Tolga <10477308+Tolga23@users.noreply.github.com>
2024-03-17 19:16:41 +0300

    Add Customer Home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {State} from "../../interface/state";
+import {CustomHttpResponse} from "../../interface/customhttpresponse";
+import {Profile} from "../../interface/profile";
+import {DataState} from "../../enum/datastate.enum";
+import {UserService} from "../../service/user.service";
+import {CustomerService} from "../../service/customer.service";
+import {Page} from "../../interface/page";
+import {User} from "../../interface/user";
+import {Customer} from "../../interface/customer";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  homeState$: Observable<State<CustomHttpResponse<Page & User>>>
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Page & User>>(null)
 
+  private isLoadingSubject = new BehaviorSubject<boolean>(false)
+  isLoading$ = this.isLoadingSubject.asObservable()
+
+  private showLogsSubject = new BehaviorSubject<boolean>(false)
+  showLogs$ = this.showLogsSubject.asObservable()
+
+  private currentPageSubject = new BehaviorSubject<number>(0)
+  currentPage$ = this.currentPageSubject.asObservable()
+
+  readonly DataState = DataState
+
+  constructor(private userService: UserService, private customerService: CustomerService) {
+  }
+
+  ngOnInit(): void {
+    this.homeState$ = this.customerService.getCustomers$()
+      .pipe(
+        map(response => {
+          console.log(response)
+          this.dataSubject.next(response)
+          return {dataState: DataState.LOADED, appData: response}
+        }),
+        startWith({dataState: DataState.LOADING}),
+        catchError((error: string) => {
+          return of({dataState: DataState.ERROR, error})
+        })
+      )
+  }
+
+  selectCustomer(customer: Customer): void {
+
+  }
 }
