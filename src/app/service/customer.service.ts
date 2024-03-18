688719c97ecf495bf9a209527b549438531f0464
Tolga <10477308+Tolga23@users.noreply.github.com>
2024-03-18 04:01:24 +0300

    Add Customer Service

---
+++ src/app/service/customer.service.ts
@@ -0,0 +1,42 @@
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {catchError, Observable, tap, throwError} from "rxjs";
+import {CustomHttpResponse} from "../interface/customhttpresponse";
+import {Page} from "../interface/page";
+import {User} from "../interface/user";
+
+
+@Injectable({providedIn: 'root'})
+export class CustomerService {
+  private readonly baseUrl: string = 'http://localhost:8080/customer'
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCustomers$ = (page: number = 0) => <Observable<CustomHttpResponse<Page & User>>>
+    this.http.get<CustomHttpResponse<Page & User>>(`${this.baseUrl}/list?page=${page}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError));
+
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage: string
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `An client error occurred: ${error.error.message}`
+      console.log(errorMessage)
+    } else {
+      if (error.error.reason) {
+        errorMessage = error.error.reason
+        console.log(errorMessage)
+      } else {
+        errorMessage = `An error occurred - Error status ${error.status}, error message is: ${error.error.message}`
+        console.log(errorMessage)
+      }
+    }
+
+    return throwError(() => error.error.message || `An error occurred - Error status ${error.status}`)
+  }
+
+}
