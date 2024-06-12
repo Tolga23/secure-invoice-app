cd408e377e4e44e24fe530574a07a636d179a8c6
Tolga Hardal <hardaltolga@gmail.com>
2024-06-13 00:34:57 +0300

    Update app module and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,28 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from "@angular/common/http";
-import {ProfileComponent} from './pages/profile/profile.component';
-import {NavbarComponent} from './pages/navbar/navbar.component';
-import {StatsComponent} from './pages/stats/stats.component';
-import {NgOptimizedImage} from "@angular/common";
 import {CoreModule} from "./core/core.module";
 import {AuthModule} from "./pages/auth/auth.module";
 import {CustomerModule} from "./pages/customer/customer.module";
 import {HomeModule} from "./pages/home/home.module";
 import {InvoiceModule} from "./pages/invoice/invoice.module";
+import {UserModule} from "./pages/profile/user.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
-    StatsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule,
     HttpClientModule,
     CoreModule,
     AuthModule,
     CustomerModule,
+    InvoiceModule,
+    UserModule,
     HomeModule,
-    InvoiceModule
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
